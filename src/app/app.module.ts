5b02eb3560cca76aa3e43a6db1167b5348ba2480
AnwarHossainSR <mahedianwar@gmail.com>
2021-06-02 20:27:16 +0600

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserlistComponent } from './userlist/userlist.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserlistComponent
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
